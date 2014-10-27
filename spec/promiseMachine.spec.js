61432ce1dd8435dfe1f8246c296a9f0d0fa1d6c8
leankitscott <scott.walters@leankit.com>
2014-10-27 17:27:33 -0400

    add test for resource acquire fail

--- spec/promiseMachine.spec.js
+++ spec/promiseMachine.spec.js
@@ -36,3 +36,47 @@ describe( 'when working with a stable resource', function() {
 	} );
 
 } );
+
+describe( 'when acquiire fails, retry should occur', function() {
+	var Not4You = function() {
+
+	};
+
+	var rejectCount = 0;
+	var factory = function() {
+		return when.promise( function( resolve, reject ) {
+			setTimeout( function() {
+				if (rejectCount < 3) {
+					rejectCount++;
+					reject( "No resource 4 You" );
+				} else {
+					resolve(new Not4You());
+				}
+			}, 100 );
+		} );
+	};
+
+	var machine = new Promy( factory, Not4You );
+	var hasFailed = false;
+	var hasAcquired = false;
+
+	machine.on('failed', function() {
+		hasFailed = true;
+	});
+	machine.on('acquired', function () {
+		if (hasFailed) {
+			hasAcquired = true;
+		}
+	});
+
+	it( 'should retry acquire', function( done ) {
+		var interval = setInterval(function () {
+			if (hasFailed && hasAcquired) {
+				clearInterval(interval);
+				done();
+			}
+		}, 200);
+	} );
+
+
+} );
