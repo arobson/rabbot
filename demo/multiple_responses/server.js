5e1da04cd98aafa699addc84690ac4dff814839a
Philipp Tusch <philipp.tusch@huf-group.com>
2019-04-16 14:24:40 +0200

    Add sample for multiple responses

---
+++ demo/multiple_responses/server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const rabbot = require('../../src/index.js');
+
+rabbot.addConnection({uri: 'amqp://shitface:shitface@localhost:5672/', replyQueue: false})
+  .then((con) => {
+    let counter = 0;
+    rabbot.handle('#', (msg) => {
+      msg.ack();
+      setTimeout(() => {
+        console.log('sending intermediate 1')
+        msg.reply({msg: 'int1', counter: counter++}, {
+          more: true,
+          headers: {},
+          replyType: '',
+        });
+      }, 100);
+
+      setTimeout(() => {
+        console.log('sending last message')
+        msg.reply({msg: 'end', counter: counter++});
+      }, 300);
+    });
+
+    return rabbot.addQueue('q.some');
+  })
+  .then(() => {
+    return rabbot.startSubscription('q.some');
+  });
